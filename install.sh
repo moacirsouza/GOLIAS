1f3ce3fb73f6166d3e709f2c53be033968483555
moacirsouza <moacir.souza@gmail.com>
2019-07-09 11:34:29 -0300

    Removing old files

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# The kernel version is need to the compilation process
-# of the CUDA driver
-export host_kernel_version="v$(uname -r)"
-
-docker build \
---build-arg HOST_KERNEL_VERSION=${host_kernel_version} \
--t moka/cuda:latest .
