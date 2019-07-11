db00d3fea81a9800bbc939a6409308b4beaf7456
moacirsouza <moacir.souza@gmail.com>
2019-07-11 11:25:01 -0300

    Creating the installation script

---
+++ IBM/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Variables passed to podman at build time
+host_arch=$(uname -i)
+host_kernel_version=$(uname -r)
+
+podman build \
+    --build-arg HOST_ARCH=${host_arch} \
+    --build-arg HOST_KERNEL_VERSION=${host_kernel_version} \
+    -t moka/cuda:0.1 .
