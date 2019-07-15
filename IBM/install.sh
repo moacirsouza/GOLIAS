6ee521179116ddfd0a37d4ec3f55f4392b448583
moacirsouza <moacir.souza@gmail.com>
2019-07-15 20:40:33 -0300

    Ditching docker for podman

--- IBM/install.sh
+++ IBM/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Shows everything that's going on while the script runs
+set -uvx
+
 # Variables passed to podman at build time
 host_arch=$(uname -i)
 host_kernel_version=$(uname -r)
 
-docker build \
+podman build \
     --build-arg HOST_ARCH=${host_arch} \
     --build-arg HOST_KERNEL_VERSION=${host_kernel_version} \
-    -t moka/cuda:0.1 .
+    -t moka/cuda:0.1 -f $1 .
